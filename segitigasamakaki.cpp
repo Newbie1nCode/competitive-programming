7d6f6bc024dcd0c2ed98e39bd6b2b9c25b80e5e2
Rianca Aril <riancaaril04@gmail.com>
2026-03-05 06:14:53 +0700

    Refactor triangle drawing logic in segitigasamakaki.cpp

--- segitigasamakaki.cpp
+++ segitigasamakaki.cpp
@@ -1,25 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    int tinggi;
-    cin >> tinggi;
-    
-    for (int i = 1; i <= tinggi; i++) {
-        // 1. Cetak Spasi dulu biar segitiga miring ke tengah
-        // Jumlah spasi = tinggi total - nomor baris sekarang
-        for (int j = 1; j <= tinggi - i; j++) {
-            cout << "  "; // 2 spasi biar rapi
-        
-        
-        // 2. Cetak Bintang
-        // Rumus jumlah bintang ganjil: 1, 3, 5, 7... (2*i - 1)
-        for (int k = 1; k <= 2 * i - 1; k++) {
-            cout << "* ";
+int main(){
+    int n;
+    cin>>n;
+
+    for(int i = 1; i <= n; i++){
+
+        // cetak spasi
+        for(int j = 1; j <= n-i; j++){
+            cout<<" ";
         }
-        
-        cout << endl;
+
+        // cetak bintang
+        for(int k = 1; k <= (2*i-1); k++){
+            cout<<"*";
+        }
+
+        cout<<endl;
     }
-    
-    return 0;
 }
